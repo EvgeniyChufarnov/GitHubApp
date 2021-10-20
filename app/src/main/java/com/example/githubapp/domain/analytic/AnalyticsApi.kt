e5597a54b6b88e7c9b657f1eac6ae64a95fb6026
Evgeniy Chufarnov <evgeniychufarnov@gmail.com>
2021-10-20 18:41:13 +0300

    hw-4: added event bus

---
+++ app/src/main/java/com/example/githubapp/domain/analytic/AnalyticsApi.kt
@@ -0,0 +1,5 @@
+package com.example.githubapp.domain.analytic
+
+interface AnalyticsApi {
+    fun onUserClicked(userName: String)
+}
---
+++ app/src/main/java/com/example/githubapp/domain/analytic/AnalyticsApiImpl.kt
@@ -0,0 +1,7 @@
+package com.example.githubapp.domain.analytic
+
+class AnalyticsApiImpl: AnalyticsApi {
+    override fun onUserClicked(userName: String) {
+
+    }
+}
