2f241ba2e2d917720982c0927f7d12be3fe5d50d
Evgeniy <evgeniychufarnov@gmail.com>
2021-11-04 18:45:21 +0300

    hw-6: set di

--- app/src/main/java/com/example/githubapp/di/RepoContainer.kt
+++ app/src/main/java/com/example/githubapp/di/RepoContainer.kt
@@ -1,15 +1,24 @@
 package com.example.githubapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.githubapp.utils.ConnectionState
+import com.example.githubapp.data.GitHubCombinedRepo
+import com.example.githubapp.data.local.GitHubLocalRepoImpl
+import com.example.githubapp.data.local.ReposDb
+import com.example.githubapp.data.local.UsersDb
 import com.example.githubapp.data.remote.GitHubApi
-import com.example.githubapp.data.remote.impls.GitHubRepoImpl
+import com.example.githubapp.data.remote.impls.GitHubRemoteRepo
 import com.example.githubapp.domain.GitHubRepo
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val GITHUB_URL = "https://api.github.com/"
+private const val GITHUB_URL = "https://api.github.com/"
+private const val USERS_DB_PATH = "users.db"
+private const val REPOS_DB_PATH = "repos.db"
 
-class RepoContainer {
+class RepoContainer(context: Context) {
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
@@ -18,5 +27,27 @@ class RepoContainer {
 
     private val gitHubApi = retrofit.create(GitHubApi::class.java)
 
-    val gitHubRepo: GitHubRepo = GitHubRepoImpl(gitHubApi)
+    private val usersDb = Room.databaseBuilder(
+        context,
+        UsersDb::class.java,
+        USERS_DB_PATH
+    ).build()
+
+    private val usersDao = usersDb.usersDao()
+
+    private val reposDb = Room.databaseBuilder(
+        context,
+        ReposDb::class.java,
+        REPOS_DB_PATH
+    ).build()
+
+    private val reposDao = reposDb.reposDao()
+
+    private val connectionState = ConnectionState(context)
+
+    val gitHubRepo: GitHubRepo = GitHubCombinedRepo(
+        GitHubRemoteRepo(gitHubApi),
+        GitHubLocalRepoImpl(usersDao, reposDao),
+        connectionState
+    )
 }
